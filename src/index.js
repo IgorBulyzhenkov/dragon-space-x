5034bd02044fba9efc380877801bcf91d84ab12c
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-09-27 16:26:16 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter basename="/">
-        {/* <PersistGate loading={<p>...Loading</p>} persistor={persistor}> */}
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter basename="/test-front/">
+      {/* <PersistGate loading={<p>...Loading</p>} persistor={persistor}> */}
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
+  </Provider>
   // </React.StrictMode>
 );
