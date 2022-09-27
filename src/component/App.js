a4ce7a070196b927ba207025285c6de9a3c864b7
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-09-27 16:45:52 +0300

    *

--- src/component/App.js
+++ src/component/App.js
@@ -5,13 +5,13 @@ import Header from "./Header/Header";
 import Gallery from "../pages/Gallery";
 
 function App() {
-
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );
