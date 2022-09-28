e4f00b923d86570bd7d924bc8eb9fe63939627ce
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-09-28 22:50:29 +0300

    gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import fetchDragon from "../redux/dragon/dragon0operation";
 import s from "./Gallery.module.css";
+import ItemsCarousel from "react-items-carousel";
+import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 
 function Gallery() {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 40;
   const dispatch = useDispatch();
 
   const data = useSelector((state) => state.dragon.items);
@@ -18,14 +22,42 @@ function Gallery() {
 
   return (
     <section className={s.carousel}>
-      <h1 className={s.isHidden}> dragon</h1>
-      <div className={s.roundabout}>
-        {arrImg?.map((el) => {
-          return <img src={el} alt={el} className={s.img} key={uuidv4()} />;
-        })}
+      <div style={{ padding: `0 ${chevronWidth}px` }}>
+        <ItemsCarousel
+          requestToChangeActive={setActiveItemIndex}
+          activeItemIndex={activeItemIndex}
+          numberOfCards={1}
+          gutter={20}
+          leftChevron={
+            <button className={s.right} type="button">
+              {<FaArrowCircleLeft className={s.svg} />}
+            </button>
+          }
+          rightChevron={
+            <button className={s.left} type="button">
+              {<FaArrowCircleRight className={s.svg} />}
+            </button>
+          }
+          outsideChevron
+          chevronWidth={chevronWidth}
+          infiniteLoop={true}
+        >
+          {arrImg?.map((el) => {
+            return <img src={el} alt={el} className={s.img} key={uuidv4()} />;
+          })}
+        </ItemsCarousel>
       </div>
     </section>
   );
 }
 
 export default Gallery;
+
+// <section className={s.carousel}>
+//   <h1 className={s.isHidden}> dragon</h1>
+//   <div className={s.roundabout}>
+//     {arrImg?.map((el) => {
+//       return <img src={el} alt={el} className={s.img} key={uuidv4()} />;
+//     })}
+//   </div>
+// </section>;
