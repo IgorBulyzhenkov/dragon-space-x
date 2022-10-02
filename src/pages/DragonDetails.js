6af615610205d5a757a4766d32442446f5eaf40a
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-10-02 21:54:13 +0300

    dateils final

--- src/pages/DragonDetails.js
+++ src/pages/DragonDetails.js
@@ -24,47 +24,51 @@ function DragonDetails() {
     <main className={s.main}>
       <Container>
         <h1 className={s.title}>{name}</h1>
-        <div className={s.containerImg}>
-          {flickr_images?.map((el) => {
-            return <img src={el} alt={el} className={s.img} key={uuidv4()} />;
-          })}
+        <div className={s.wrap}>
+          <div className={s.containerImg}>
+            {flickr_images?.map((el) => {
+              return <img src={el} alt={el} className={s.img} key={uuidv4()} />;
+            })}
+          </div>
+          <div className={s.listContainer}>
+            <ul className={s.list}>
+              <li className={s.item}>
+                <p className={s.text}>Active:</p>
+                <span className={s.span}>{active ? "yes" : "no"}</span>
+              </li>
+              <li className={s.item}>
+                <p className={s.text}>First flight:</p>
+                <span className={s.span}>{first_flight}</span>
+              </li>
+              <li className={s.item}>
+                <p className={s.text}>Type:</p>
+                <span className={s.span}>{type}</span>
+              </li>
+              <li className={s.item}>
+                <p className={s.text}>Dry mass kg:</p>
+                <span className={s.span}>{dry_mass_kg}kg</span>
+              </li>
+              <li className={s.item}>
+                <p className={s.text}>Height w trunk:</p>
+                <span className={s.span}>{height_w_trunk?.meters}m</span>
+              </li>
+              <li className={s.item}>
+                <a
+                  href={wikipedia}
+                  target="_blank"
+                  rel="noreferrer"
+                  className={s.link}
+                >
+                  <span className={s.span}>Wikipedia:</span> {name}
+                </a>
+              </li>
+              <li className={s.item}>
+                <p className={s.text}> Description: </p>
+                <span className={s.span}>{description}</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <ul className={s.list}>
-          <li className={s.item}>
-            <p className={s.text}>Active:</p>
-            <span className={s.span}>{active ? "yes" : "no"}</span>
-          </li>
-          <li className={s.item}>
-            <p className={s.text}>First flight:</p>
-            <span className={s.span}>{first_flight}</span>
-          </li>
-          <li className={s.item}>
-            <p className={s.text}>Type:</p>
-            <span className={s.span}>{type}</span>
-          </li>
-          <li className={s.item}>
-            <p className={s.text}>Dry mass kg:</p>
-            <span className={s.span}>{dry_mass_kg}kg</span>
-          </li>
-          <li className={s.item}>
-            <p className={s.text}>Height w trunk:</p>
-            <span className={s.span}>{height_w_trunk?.meters}m</span>
-          </li>
-          <li className={s.item}>
-            <a
-              href={wikipedia}
-              target="_blank"
-              rel="noreferrer"
-              className={s.link}
-            >
-              <span className={s.span}>Wikipedia:</span> {name}
-            </a>
-          </li>
-          <li className={s.item}>
-            <p className={s.text}> Description: </p>
-            <span className={s.span}>{description}</span>
-          </li>
-        </ul>
       </Container>
     </main>
   );
