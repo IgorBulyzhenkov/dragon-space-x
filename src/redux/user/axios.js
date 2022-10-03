65d6888ff85b526b3ccf984d99751c4212dbc405
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-10-03 12:42:21 +0300

    delete comment

--- src/redux/user/axios.js
+++ src/redux/user/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://igor-bulyzhenkov.herokuapp.com/api/users";
-// axios.defaults.baseURL = "http://localhost:4000/api/users";
+
 export const token = {
   set(token) {
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
