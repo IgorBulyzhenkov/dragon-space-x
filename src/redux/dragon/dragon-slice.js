5144d233627ecd353f1b6652b19143b2f5ccd162
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-10-05 21:26:49 +0300

    fix slice

--- src/redux/dragon/dragon-slice.js
+++ src/redux/dragon/dragon-slice.js
@@ -6,9 +6,7 @@ const { fetchDragon, fetchDragonId } = dragonOperation;
 const initialState = {
   items: [],
   itemsId: [],
-  isLoggedIn: false,
   error: null,
-  isRefreshing: false,
 };
 
 const dragon = createSlice({
@@ -17,31 +15,23 @@ const dragon = createSlice({
   extraReducers: {
     [fetchDragon.fulfilled](state, { payload }) {
       state.items = payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
+      state.error = null;
     },
     [fetchDragonId.fulfilled](state, { payload }) {
       state.itemsId = payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
+       state.error = null;
     },
     [fetchDragon.rejected](state, { payload }) {
       state.error = payload;
-      state.isRefreshing = false;
     },
     [fetchDragonId.rejected](state, { payload }) {
       state.error = payload;
-      state.isRefreshing = false;
     },
     [fetchDragon.pending](state) {
-      state.isLoggedIn = true;
       state.error = null;
-      state.isRefreshing = false;
     },
     [fetchDragonId.pending](state) {
-      state.isLoggedIn = true;
       state.error = null;
-      state.isRefreshing = false;
     },
   },
 });
